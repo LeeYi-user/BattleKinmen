e646c5e5ff57fa4acd6a88d5ed25e6e01c2ee89b
LeeYi-user <khes60122@gmail.com>
2023-12-01 22:16:13 +0800

    change directory

--- Assets/Scripts/WeaponSway.cs
+++ Assets/Scripts/WeaponSway.cs
@@ -13,18 +13,17 @@ public class WeaponSway : MonoBehaviour
     [SerializeField] private float multiplier;
     [SerializeField] private float originalRotationY;
 
-    private bool live = true;
-
     // Update is called once per frame
     private void Update()
     {
-        if (Cursor.lockState == CursorLockMode.None)
-        {
-            return;
-        }
+        float mouseX = originalRotationY;
+        float mouseY = 0f;
 
-        float mouseX = Input.GetAxisRaw("Mouse X") * multiplier + originalRotationY;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * multiplier;
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            mouseX += Input.GetAxisRaw("Mouse X") * multiplier;
+            mouseY += Input.GetAxisRaw("Mouse Y") * multiplier;
+        }
 
         Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
         Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
