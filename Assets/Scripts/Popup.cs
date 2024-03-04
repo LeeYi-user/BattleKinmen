247aea2b6144c00ba61e72e2a7940b0521c17c70
LeeYi-user <khes60122@gmail.com>
2024-03-04 14:10:31 +0800

    add translate to popup

--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -5,8 +5,10 @@ using TMPro;
 
 public class Popup : MonoBehaviour
 {
+    [SerializeField] private RectTransform rectTransform;
     [SerializeField] private TextMeshProUGUI text;
 
+    private bool translating;
     private Color targetColor;
     private float timeLeft;
 
@@ -20,6 +22,12 @@ public class Popup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!translating)
+        {
+            translating = true;
+            StartCoroutine(Translate(new Vector3(0, 75, 0), 0.25f));
+        }
+
         if (timeLeft <= Time.deltaTime)
         {
             text.color = targetColor;
@@ -30,5 +38,19 @@ public class Popup : MonoBehaviour
             text.color = Color.Lerp(text.color, targetColor, Time.deltaTime / timeLeft);
             timeLeft -= Time.deltaTime;
         }
+    }
+
+    private IEnumerator Translate(Vector3 deltaPosition, float duration)
+    {
+        Vector3 startPosition = rectTransform.localPosition;
+        Vector3 endPosition = startPosition + deltaPosition;
+
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            rectTransform.localPosition = Vector3.Lerp(startPosition, endPosition, t / duration);
+            yield return null;
+        }
+
+        rectTransform.localPosition = endPosition;
     }
 }
