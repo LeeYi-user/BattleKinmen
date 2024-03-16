bc95f5cb25a424f29620b6717f5ef9b1a77bb31e
LeeYi-user <khes60122@gmail.com>
2024-03-17 01:32:46 +0800

    add weapon sway to knife

--- Assets/Scripts/WeaponSway.cs
+++ Assets/Scripts/WeaponSway.cs
@@ -7,12 +7,14 @@ public class WeaponSway : MonoBehaviour
     [Header("Sway Settings")]
     [SerializeField] private float smooth;
     [SerializeField] private float multiplier;
-    [SerializeField] private float originalRotationZ;
+    [SerializeField] private float baseRotationX;
+    [SerializeField] private float baseRotationY;
+    [SerializeField] private float baseRotationZ;
 
     private void Update()
     {
-        float mouseX = 0;
-        float mouseY = 0;
+        float mouseX = baseRotationY;
+        float mouseY = baseRotationX;
 
         if (Cursor.lockState == CursorLockMode.Locked)
         {
@@ -22,7 +24,7 @@ public class WeaponSway : MonoBehaviour
 
         Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
         Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
-        Quaternion rotationZ = Quaternion.AngleAxis(originalRotationZ, Vector3.forward);
+        Quaternion rotationZ = Quaternion.AngleAxis(baseRotationZ, Vector3.forward);
         Quaternion targetRotation = rotationX * rotationY * rotationZ;
 
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
